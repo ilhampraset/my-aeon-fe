0cecc950721cf5118ff8ae2984c1913687260a4f
Ilham Eka <ilham.eka@majoo.id>
2024-03-17 02:04:38 +0700

    feat:about

---
+++ src/views/About.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header ">
+            <h5 class="card-title">Admin Dashboard</h5>
+          </div>
+          <div class="card-body" >
+            <p class="about-description"> The admin dashboard for user management provides essential functionalities for managing users efficiently. It includes features such as user listing, creation, deletion, and updating user information. Additionally, it may offer user role management, allowing administrators to assign roles and permissions to users. The dashboard often includes search and filtering options to quickly find specific users. User activity tracking and logging may also be incorporated to monitor user actions within the system. Overall, the user management system aims to streamline administrative tasks related to user accounts while ensuring security and accessibility.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style>
+.about-description {
+  font-family: 'Mulish', sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0px;
+  color: #000000;
+}
+</style>
