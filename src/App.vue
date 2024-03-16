d63d2f326cced8d39dfb82c32506f0057890dd9e
Ilham Eka <ilham.eka@majoo.id>
2024-03-17 02:03:44 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Sidebar :isSidebarOpen="isSidebarOpen"  @closeSidebar="closeSidebar" ></Sidebar>
+  <div id="content">
+  <Navbar :toggleSidebar="toggleSidebar"></Navbar>
+  <MainContent></MainContent>
+  <Footer></Footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from './components/Navbar.vue'
+import Sidebar from "@/components/Sidebar.vue";
+import MainContent from "@/components/MainContent.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Sidebar,
+    Navbar,
+    MainContent,
+    Footer
+  },
+  data() {
+    return {
+      isSidebarOpen: false
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.isSidebarOpen = !this.isSidebarOpen;
+    },
+    closeSidebar() {
+      this.isSidebarOpen = false;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.sidebar {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 100;
+  padding: 0px 0 0;
+  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
+  z-index: 99;
+  background-color :#363740;
+}
+
+@media (max-width: 768px) {
+  .sidebar {
+    padding: 0;
+  }
+}
+
+.navbar {
+  box-shadow: inset 0 -1px 0 rgba(0, 0, 0, .1);
+}
+
+@media (min-width: 768px) {
+  .navbar {
+    top: 0;
+    position: sticky;
+    z-index: 999;
+  }
+}
+
+.sidebar .nav-link {
+  color: #A4A6B3;
+}
+
+.sidebar .nav-link.active {
+  background-color: #505050;
+}
+body {
+  background-color: #F7F8FC;
 }
 </style>
+
